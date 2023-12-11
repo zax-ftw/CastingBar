f4223daf219c8ebc9b672af8425950d7574eb83e
_kmatt_ <124720007+kcmatthe@users.noreply.github.com>
2023-12-11 16:39:43 -0600

    Added missing declarations
    Added declarations for two variables added in MagicStat.cpp

---
+++ MagicState.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "IState.h"
+
+class MagicState : public IState
+{
+public:
+	using SlotType = RE::MagicSystem::CastingSource;
+
+	//enum CastState : std::uint32_t
+	//{
+	//	kNone = 0,
+	//	kUnk1 = 1,
+	//	kCharging = 2,
+	//	kCharged = 3,
+	//	kRelease = 4,
+	//	kUnk5 = 5,
+	//	kConcentrating = 6
+	//};
+
+	MagicState(SlotType a_slot);
+
+	std::unique_ptr<IState> HandleAction(RE::Actor* actor, Action action) override;
+	std::optional<float> GetProgress(RE::PlayerCharacter* player) override;
+
+	float castingTime = 0;
+	int i = 0;
+
+private:
+
+	RE::MagicCaster* GetMagicCaster(RE::Actor* actor);
+
+	SlotType slot;
+};
